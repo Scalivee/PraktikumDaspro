56db4806fc0cd3930ae7ac81b0a771453d74eb66
unknown <cahyowhn04@gmal.com>
2024-09-06 10:00:20 +0700

    praktikum01

---
+++ Praktikum01/Latihan21.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan21
+ */
+public class Latihan21 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Sucahyo");
+        System.out.print("\nsaya adalah mahasiswa Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Praktikum01/Tugas1.java
@@ -0,0 +1,14 @@
+public class Tugas1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("..");
+        System.out.println("****IDENTITAS DIRI****");
+        System.out.println("Nama    : Sucahyo Dwiwahono");
+        System.out.println("Absen   : 21");
+        System.out.println("Kelas   : SIB 1D");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+    
+}
