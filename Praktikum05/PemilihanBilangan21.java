10867b34917272458c13c22c08a7bebf21d71a32
unknown <cahyowhn04@gmal.com>
2024-09-25 13:22:48 +0700

    Praktikum05

--- Praktikum05/PemilihanBilangan21.java
+++ Praktikum05/PemilihanBilangan21.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PemilihanBilangan21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        sc.close();
         System.out.println(",");
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
