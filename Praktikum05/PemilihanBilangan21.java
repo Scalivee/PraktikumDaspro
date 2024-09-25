125cdf0d75b8b3412a27277df515b4d25d9f8bd5
unknown <cahyowhn04@gmal.com>
2024-09-25 14:02:02 +0700

    Praktikum05

--- Praktikum05/PemilihanBilangan21.java
+++ Praktikum05/PemilihanBilangan21.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PemilihanBilangan21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.close();
+        
         System.out.println(",");
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
@@ -19,7 +19,11 @@ public class PemilihanBilangan21 {
         }
 
 
+            //Bentuk ternary
+      //  System.out.println("Angka " + angka + " termasuk bilangan " + (angka % 2 == 0 ? "genap" : "ganjil"));
 
 
+        sc.close();
     }
+    
 }
--- Praktikum05/PemilihanHari21.java
+++ Praktikum05/PemilihanHari21.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PemilihanHari21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        sc.close();
         String dayName, dayType;
 
         System.out.println(",");
